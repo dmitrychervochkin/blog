402913857218eab3023f63dfacded28c53d178cd
Dmitry Chervochkin <chervochkin2001@mail.ru>
2024-02-05 19:31:26 +0300

    Заполнение базы данных

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export const App = () => {
 		<Div className="App">
 			<div>123</div>
 			<i className="fa fa-address-book" aria-hidden="true"></i>
-			<i class="fa fa-battery-three-quarters" aria-hidden="true"></i>
+			<i className="fa fa-battery-three-quarters" aria-hidden="true"></i>
 		</Div>
 	);
 };
+// json-server --watch src/db.json --port 3005
